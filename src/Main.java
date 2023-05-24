0ad1ec087229c2dcee53930e46a70524dde0b043
Wladimir <weizen.w@gmx.de>
2023-05-24 08:28:38 +0200

    feat: add template Main, add empty methods to Class Atm, Account, Bank, Client, Transaction. Rename files. Remove Iatm.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,35 @@
+import atm.Atm;
+import atm.Bank;
+import atm.Client;
+import java.util.Scanner;
+
 public class Main {
 
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Atm atm = new Atm();
+    int choiceMain;
+    int choiceSub;
+    do {
+      atm.printMenu();
+      choiceMain = atm.selectMenu(scanner);
+      if (choiceMain == 1) {
+        Client client = Bank.authorization(scanner);
+        do {
+          atm.printMenu();
+          choiceSub = atm.selectMenu(scanner);
+          switch (choiceSub) {
+            case 1 -> client.selectAccount(scanner).payment(scanner);
+            case 2 -> client.selectAccount(scanner).transfer(scanner);
+            case 3 -> client.selectAccount(scanner).deposit(scanner);
+            case 4 -> client.selectAccount(scanner).withdraw(scanner);
+            case 5 -> System.out.println(client.balance());
+            case 6 -> client.printHistory();
+            case 7 -> Bank.changePIN(client);
+          }
+        } while (choiceSub != 0);
+      }
+    } while (choiceMain != 0);
+    scanner.close();
+  }
 }
--- src/atm/Iatm.java
+++
@@ -1,5 +0,0 @@
-package atm;
-
-public interface Iatm {
-
-}
