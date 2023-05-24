aacec0d7f61b7da8f86e146e6806a0d9f935ca32
Wladimir <weizen.w@gmx.de>
2023-05-24 15:13:46 +0200

    feat: fill tests.BankTests

--- src/tests/BankTests.java
+++ src/tests/BankTests.java
@@ -1,5 +1,40 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import atm.Bank;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class BankTests {
 
+  @Test
+  public void testConstructor() {
+    // arrange
+    String name = "ProjectBank";
+    String address = "10178 Berlin, Panoramastr. 1A";
+    // arrange & act
+    Bank bank = new Bank(name, address);
+    // assert
+    assertEquals(name, bank.getName()); // TODO
+    assertEquals(address, bank.getAddress()); // TODO
+  }
+
+  @ParameterizedTest
+  @CsvSource({
+      "'ProjectBank', '10178 Berlin, Panoramastr. 1A'",
+  })
+  public void testToString(String name, String address) {
+    // arrange
+    Bank bank = new Bank(name, address);
+    String expected = "Bank{" +
+        "name='" + name + '\'' +
+        ", address='" + address + '\'' +
+        '}';
+    // act
+    String actual = bank.toString();
+    // assert
+    assertEquals(expected, actual);
+  }
 }
