6c646eb863a437f5ec1be8f51c53b5802ec43915
Oleksandr Zhurba <starkyzh@gmail.com>
2023-05-25 07:28:30 +0200

    fix: clean import

--- src/atm/Transaction.java
+++ src/atm/Transaction.java
@@ -1,15 +1,7 @@
 package atm;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.Scanner;
 
 public class Transaction {
 
